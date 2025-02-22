e6171603996221c60ced15a869009e9d9d17444d
vanderleik <vanderleik@yahoo.com.br>
2025-02-22 08:11:35 -0300

    initial commit

---
+++ src/main/java/com/api/crud_de_clientes/CrudDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.api.crud_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/crud_de_clientes/CrudDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.crud_de_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
