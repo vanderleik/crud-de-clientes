a1af5824f0069bddf670204a455a08096c5194e9
vanderleik <vanderleik@yahoo.com.br>
2025-02-23 19:36:34 -0300

    feat: adicionando erro 404 ao buscar ID que não existe

---
+++ src/main/java/com/api/crud_de_clientes/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.api.crud_de_clientes.controllers.handlers;
+
+import com.api.crud_de_clientes.dtos.CustomError;
+import com.api.crud_de_clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
